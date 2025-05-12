0da2d388465fca6107b2ca4c05be8ecdf5836125
rshelke-academian <rshelke@academian.com>
2025-05-12 21:48:36 +0530

    change in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "lambda-deployment21"           # Your existing bucket
+    bucket = "${{ secrets.S3_BUCKET }}"           # Your existing bucket
     key    = "terraform/chatbot-lambda.tfstate"  # Path inside bucket
     region = "us-east-1"
   }
