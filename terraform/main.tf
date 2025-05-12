bc87029d5fc2dfc8cd19257e0a51b561b6910272
rshelke-academian <rshelke@academian.com>
2025-05-12 23:09:59 +0530

    removed duplicate variables from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,11 +21,6 @@ data "aws_s3_bucket" "lambda_bucket" {
 }
 
 # Conditional IAM role creation
-variable "create_lambda_role" {
-  type    = bool
-  default = false
-}
-
 resource "aws_iam_role" "lambda_role" {
   count = var.create_lambda_role ? 1 : 0
   name  = var.lambda_role_name
